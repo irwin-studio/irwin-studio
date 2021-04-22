f372fef77f3c0d5fee0bf02e90a4d1e155f5ec03
Gogs <gogs@fake.local>
2021-04-22 18:40:08 +1000

    feat(tailwind): add tailwind configuration

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BaseStyle from './styles';
 import App from './containers/App';
-import breakpoints from './breakpoints';
 import {BreakpointContext} from './services/BreakPoints';
+import tailwind from '../tailwind.js';
+
+const breakpoints: Record<string, number> = {};
+Object.entries(tailwind.theme.screens).map(([name, value]: [string, string]) => {
+    breakpoints[name] = Number.parseInt(value.replace('px', ''), 10);
+});
 
 ReactDOM.render(
     <BreakpointContext breakpoints={breakpoints}>
