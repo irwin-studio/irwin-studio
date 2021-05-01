78b6a0a481c5b507c267db6e0411cbf567a5da65
Gogs <gogs@fake.local>
2021-05-01 12:14:49 +1000

    feat(cosmos): integrate cosmos and create first fixture

---
+++ src/components/Splash/index.fixture.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Splash from '.';
+
+export default (
+    <Splash>
+        <p>I&apos;m a &lt;p&gt; tag</p>
+        <h4>
+            Fill this component with anything
+            <br />
+            and it will be automatically centered.
+        </h4>
+    </Splash>
+);
