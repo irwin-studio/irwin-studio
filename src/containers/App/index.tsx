3e49a7abfeeecebedb842fd02570d2dc018eb471
lauchlan105 <lauchlan.irwin@gmail.com>
2021-05-30 00:22:54 +1000

    feat(tree-graph-view): add container and routes to view tree graphs

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import {Redirect, Switch} from 'react-router';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import TreeGraph from '../TreeGraph';
 import {ComingSoon} from '../Splash';
 
-const App: React.FC = () => <ComingSoon />;
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={ComingSoon} />
+                <Route path="/graph/tree" exact component={TreeGraph} />
+                <Route>
+                    <Redirect to="/" />
+                </Route>
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
