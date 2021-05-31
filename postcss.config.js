317b35ef17a34f33751ac1a15fcd91ba34a67abd
lauchlan105 <lauchlan.irwin@gmail.com>
2021-05-31 19:48:23 +1000

    build(minification): add js minification and tailwind purging

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,15 @@
 const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: ['./src/**/*.[jt]sx', './src/**/*.[jt]s', './build/index.html'],
+    css: ['./src/assets/tailwind/tailwind.css'],
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
 
 module.exports = {
     plugins: [
         tailwindcss('./tailwind.js'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        purgecss,
     ],
 };
