b6ba384a1268a9f650f2cb05fe6eb900afa72f7f
lauchlan105 <lauchlan.irwin@gmail.com>
2021-06-22 08:50:57 +1000

    refactor(monorepo): move frontend to monorepo package

---
+++ packages/frontend/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+        ecmaFeatures: {
+            jsx: true, // Allows for the parsing of JSX
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+    rules: {
+        'react/prop-types': 0,
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            {vars: 'all', args: 'after-used', ignoreRestSiblings: false},
+        ],
+        'no-unused-vars': 'off',
+    },
+    overrides: [
+        {
+            files: ['bin/*.js', 'lib/*.js'],
+            extends: [
+                'plugin:react/recommended',
+                'plugin:@typescript-eslint/recommended',
+                'prettier/@typescript-eslint',
+                'plugin:prettier/recommended',
+            ],
+        },
+    ],
+};
