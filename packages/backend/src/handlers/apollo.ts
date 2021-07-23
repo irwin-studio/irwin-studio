136aabbb4127dfc202d329045872d88101faec3a
lauchlan105 <lauchlan.irwin@gmail.com>
2021-07-24 09:21:05 +1000

    feat(backend): add debug header to get see raw exceptions

--- packages/backend/src/handlers/apollo.ts
+++ packages/backend/src/handlers/apollo.ts
@@ -1,24 +1,31 @@
-import {ApolloServer, Config} from 'apollo-server-cloudflare';
+import {ApolloServer, Config as Config} from 'apollo-server-cloudflare';
 import {graphqlCloudflare} from 'apollo-server-cloudflare/dist/cloudflareApollo';
-
+import {Context} from '../types/context';
 import KVCache from '../cache';
 import {GqlHandlerOptions} from '../types/handler';
 import {typeDefs, resolvers} from '../schema';
+import {isDebugRequest} from '..';
 
 const dataSources = () => ({});
 
-const createServer = (graphQLOptions: GqlHandlerOptions) =>
-    new ApolloServer({
+const createServer = (graphQLOptions: GqlHandlerOptions, debug: boolean) => {
+    const context: Context = {
+        DEBUG: debug,
+    };
+
+    return new ApolloServer({
         typeDefs,
         resolvers,
         introspection: true,
-        // @ts-ignore
         dataSources,
         ...(graphQLOptions.kvCache ? {cache: new KVCache()} : {}),
+        context,
     } as Config);
+};
 
 export default (request: Request, graphQLOptions: GqlHandlerOptions): Response => {
-    const server = createServer(graphQLOptions);
+    const debug = graphQLOptions.allowDebug && isDebugRequest(request);
+    const server = createServer(graphQLOptions, debug);
 
     // @ts-ignore
     return graphqlCloudflare(() => server.createGraphQLServerOptions(request))(request);
