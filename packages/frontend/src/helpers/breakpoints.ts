bfe3b395bb5693c1a81b2d595b06700b3649f720
lauchlan105 <lauchlan.irwin@gmail.com>
2021-07-25 20:23:55 +1000

    refactor(frontend): remove tailwindcss, related scripts and related imports

--- packages/frontend/src/helpers/breakpoints.ts
+++
@@ -1,8 +0,0 @@
-import tailwind from '../../tailwind.js';
-
-const breakpoints: Record<string, number> = {};
-Object.entries(tailwind.theme.screens).map(([name, value]: [string, string]) => {
-    breakpoints[name] = Number.parseInt(value.replace('px', ''), 10);
-});
-
-export default breakpoints;
