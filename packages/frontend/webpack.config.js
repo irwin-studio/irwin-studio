d26846bcca90e6d84b3f6c062c753b37c672554a
lauchlan105 <lauchlan.irwin@gmail.com>
2021-07-26 17:18:31 +1000

    feat(frontend): implement firebase context provider

--- packages/frontend/webpack.config.js
+++ packages/frontend/webpack.config.js
@@ -57,6 +57,15 @@ module.exports = (env, argv) => {
         plugins: [
             new DefinePlugin({
                 IS_TESTING: undefined,
+                FIREBASE_CONFIG: JSON.stringify({
+                    apiKey: process.env.FB_API_KEY,
+                    authDomain: process.env.FB_AUTH_DOMAIN,
+                    projectId: process.env.FB_PROJECT_ID,
+                    storageBucket: process.env.FB_STORAGE_BUCKET,
+                    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+                    appId: process.env.FB_APP_ID,
+                    measurementId: process.env.FB_MEASUREMENT_ID,
+                }),
             }),
             new HtmlWebpackPlugin({
                 favicon: './src/assets/favicon.svg',
