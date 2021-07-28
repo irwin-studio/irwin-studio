bf2d84c9d238e32626b972dcbe82754c33caa2ec
lauchlan105 <lauchlan.irwin@gmail.com>
2021-07-28 21:40:21 +1000

    refactor(frontend): move breakpoints service to hook

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -2,26 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BaseStyle from './styles';
 import App from './containers/App';
-import {BreakpointContext} from './services/BreakPoints';
 import {FirebaseContext} from './services/Firebase';
 import {AuthContext} from './services/Auth';
 
-export const breakpoints = {
-    sm: 640,
-    md: 768,
-    lg: 1024,
-    xl: 1280,
-    '2xl': 1536,
-};
-
 ReactDOM.render(
-    <BreakpointContext breakpoints={breakpoints}>
-        <FirebaseContext>
-            <AuthContext>
-                <BaseStyle />
-                <App />
-            </AuthContext>
-        </FirebaseContext>
-    </BreakpointContext>,
+    <FirebaseContext>
+        <AuthContext>
+            <BaseStyle />
+            <App />
+        </AuthContext>
+    </FirebaseContext>,
     document.getElementById('root'),
 );
