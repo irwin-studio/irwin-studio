2506f93107790be2ba0b4282025b2ddd9c50b4a4
lauchlan105 <lauchlan.irwin@gmail.com>
2021-10-04 20:27:58 +1100

    feat(frontend): add all components for renderers

--- packages/frontend/src/containers/App/index.tsx
+++ packages/frontend/src/containers/App/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Redirect, Switch} from 'react-router';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import TreeGraph from '../TreeGraph';
 import {ComingSoon} from '../Splash';
 import Login from '../Login';
+import Renderer from '../Renderer';
 
 const App: React.FC = () => {
     return (
@@ -12,6 +12,7 @@ const App: React.FC = () => {
                 <Route path="/" exact component={ComingSoon} />
                 <Route path="/graph/tree" exact component={TreeGraph} />
                 <Route path="/login" exact component={Login} />
+                <Route path="/experimental/webgpu/:rendererName" exact component={Renderer} />
                 <Route>
                     <Redirect to="/" />
                 </Route>
