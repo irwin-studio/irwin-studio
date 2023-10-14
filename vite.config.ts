621d5c7ba97a041dafa9cf3c797d59f14ddfe1f7
lauchlan105 <lauchlan.irwin@gmail.com>
2023-10-14 17:38:23 +1100

    chore(vitest): exclude playwright tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+	test: {
+		exclude:[
+      ...configDefaults.exclude, 
+      'tests/*'
+    ]
+	}
 });
